505aba9ce81d19b90b20ae4fe0fe1cc82f59de2b
mohamed <mohammedhosny53@gmail.com>
2025-03-09 07:37:03 +0200

    create styles and add title to home view

---
+++ lib/features/home/presentation/views/widgets/featured_list_view.dart
@@ -0,0 +1,20 @@
+import 'package:bookly/features/home/presentation/views/widgets/featured_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: const FeaturedListViewItem(),
+        ),
+      ),
+    );
+  }
+}
