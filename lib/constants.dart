5e54c1ac638ef915474a67451ce24f37810e90b9
mohamed <mohammedhosny53@gmail.com>
2025-03-09 06:15:51 +0200

    refactor init state & navigator to home

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,5 @@
 import 'dart:ui';
 
 const kPrimaryColor = Color(0xff100B20);
+
+const kTransitionDuration = Duration(milliseconds: 250);
--- lib/features/splash/presentaton/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentaton/views/widgets/splash_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splash/presentaton/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -16,14 +20,8 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(vsync: this, duration: const Duration(seconds: 2));
-
-    slidingAnimation = Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero).animate(animationController);
-
-    animationController.forward();
-    // slidingAnimation.addListener(() {
-    //   setState(() {});
-    // });
+    initSlidingAnimation();
+    navigateToHome();
   }
 
   @override
@@ -42,5 +40,23 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
         SlidingText(slidingAnimation: slidingAnimation,),
       ],
     );
+  }
+
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+
+    slidingAnimation = Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero).animate(animationController);
+
+    animationController.forward();
+    // slidingAnimation.addListener(() {
+    //   setState(() {});
+    // });
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(() => const HomeView(), transition: Transition.fade, duration: kTransitionDuration);
+    });
   }
 }
