1dfeea43ed08378187c526fa2272436fbe5691d5
mohamed <mohammedhosny53@gmail.com>
2025-03-11 03:51:33 +0200

    add rating to details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
@@ -27,6 +28,8 @@ class BookDetailsViewBody extends StatelessWidget {
               fontWeight: FontWeight.w500
             )),
           ),
+          const SizedBox(height: 18),
+          BookRating(mainAxisAlignment: MainAxisAlignment.center,),
         ],
       ),
     );
