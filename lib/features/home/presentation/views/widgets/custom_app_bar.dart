49b96ccff0e1b8bbc9ea138692a0ff8537466ed8
mohamed <mohammedhosny53@gmail.com>
2025-03-11 07:47:59 +0200

    create custom search text field

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,5 +1,7 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -12,7 +14,9 @@ class CustomAppBar extends StatelessWidget {
         children: [
           Image.asset(AssetsData.logo, height: 18),
           const Spacer(),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search, size: 30)),
+          IconButton(onPressed: () {
+            GoRouter.of(context).push(AppRouter.kSearchView);
+          }, icon: const Icon(Icons.search, size: 30)),
         ],
       ),
     );
---
+++ lib/features/search/presentation/views/widgets/custom_search_text_field.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchTextField extends StatelessWidget {
+  const CustomSearchTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        hintText: 'Search for books',
+        suffixIcon: IconButton(onPressed: () {}, icon:const Opacity(opacity: .8, child: Icon(Icons.search))),
+        enabledBorder: buildOutlineInputBorder(),
+        focusedBorder: buildOutlineInputBorder(),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildOutlineInputBorder() {
+    return OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.white),
+      borderRadius: BorderRadius.circular(12),
+    );
+  }
+}
+
